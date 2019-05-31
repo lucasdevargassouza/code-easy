66fc228b4e0e60d108fbd3b116a08dd471768a8d
Lucas DeVargas Souza <ls18017@gmail.com>
2019-05-30 23:12:43 -0300

    Adicionado biblioteca de comandos e está criando projeto pasta escolhida!

--- src/app.js
+++ src/app.js
@@ -1,8 +1,48 @@
-const pages = require('./pages/index');
-const share = require('./share/index');
-const Button = require('./share/components/button/index')
+const { app, BrowserWindow } = require('electron')
+const Shell = require('node-powershell');
+const { dialog } = require('electron').remote
+const ps = new Shell({ executionPolicy: 'Bypass', noProfile: true });
 
-// Inicializa a aplicação
-(function() {
-    document.appendChild(Button); // Errado
-})();
+
+// var locais.
+var folderPath;
+
+function startProject() {
+
+    if(document.getElementById('inputProjectname').value == "" || document.getElementById('inputProjectname').value == undefined) {
+        alert("Digite o nome do projeto antes de continuar!");
+        return;
+    }
+
+
+    alert("Começou na pasta: " + folderPath);
+    ps.addCommand('cd ' + folderPath);
+    ps.addCommand('ng new ' + document.getElementById('inputProjectname').value);
+    ps.invoke()
+        .then(output => {
+            console.log(output);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+// Seleciona a pasta onde será criado o projeto!
+function openFolder() {
+    folderPath = dialog.showOpenDialog({ properties: ['openDirectory'] })[0]
+}
+
+
+
+
+
+
+
+/* ps.addCommand('echo node-powershell');
+ps.invoke()
+.then(output => {
+  console.log(output);
+})
+.catch(err => {
+  console.log(err);
+}); */
